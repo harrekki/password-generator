e58a56bb21ef62fd371e748abf49e158c9a32b47
David LaRocco <dlarocco77@gmail.com>
2023-10-04 12:31:32 -0600

    add functionality for no duplicate chars in passwords

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function generatePasswords() {
         passwordsArr.length = 0;
     }
     const passwdLength = pwLength.value;
-    // Determine if numbers and/or symbols are included
+    
     let arr = letters;
     if(pwNumbers.checked) {
         arr = arr.concat(numbers);
@@ -30,12 +30,21 @@ function generatePasswords() {
     const numOfPasswds = 4;
     
     for(let j = 0; j < numOfPasswds; j++) {
-        let password = "";
-        for(let i = 0; i < passwdLength; i++) {
-            const randomIndex = Math.floor(Math.random() * arr.length);
-            password += arr[randomIndex];
+        let password = []; 
+        for(let i = 0; i < passwdLength; i++) { 
+            let randomIndex = Math.floor(Math.random() * arr.length);
+            if(pwDuplicates.checked){
+                // while password has a value equal to arr[randomIndex]
+                while(password.includes(arr[randomIndex])) {
+                    //generate a new random number for randomIndex
+                    randomIndex = Math.floor(Math.random() * arr.length);
+                }
+            }
+            password.push(arr[randomIndex]);
         }
-        passwordsArr.push(password);
+        // convert password from array to string
+        const passwordStr = password.join('');
+        passwordsArr.push(passwordStr);
     }
 }
 
